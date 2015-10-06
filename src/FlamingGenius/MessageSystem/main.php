db5f988a065bda58609205b0530b10186441fb04
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-06 15:30:34 -0400

    Update main.php

--- src/FlamingGenius/MessageSystem/main.php
+++ src/FlamingGenius/MessageSystem/main.php
@@ -19,73 +19,14 @@ class main extends PluginBase{
 
  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
   $cmd = $command->getName();
-  $msg = implode(" " , $args);
-  $sc = $this->getConfig()->get("broadcast-shortcut");
-  $player = $this->getServer()->getPlayer()->getName();
-  $permission = $this->getConfig->get("default-permission");
-  if($player->hasPermission($permission){
-   if(strtolower($cmd) == "ms")
-    if($args[0] == $sc){
-     $tag = $this->getConfig()->get("broadcast-tag");
-     $color = $this->getConfig()->get("broadcast-color");
-     $mcolor = $this->getConfig()->get("brm-color");
-     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-    }
-   
-   elseif(strtolower($cmd) == $sc){
-    $tag = $this->getConfig()->get("bgod-tag");
-    $sc = $this->getConfig->()->get("bgod-shortcut");
-    $color = $this->getConfig()->get("bgod-color");
-    $mcolor= $this->getConfig()->get("bgm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   }
-
-   elseif(strtolower($cmd) == $sc){
-    $sc = $this->getConfig->()->get("bhero-shortcut");
-    $tag = $this->getConfig()->get("bhero-tag");
-    $color = $this->getConfig()->get("bhero-color");
-    $mcolor= $this->getConfig()->get("bhm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   }
-
-   elseif(strtolower($cmd) == $sc){
-    $sc = $this->getConfig->()->get("announce-shortcut");
-    $tag = $this->getConfig()->get("announce-tag");
-    $color = $this->getConfig()->get("announce-color");
-    $mcolor= $this->getConfig()->get("anm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   }
-
-   elseif(strtolower($cmd) == $sc){
-    $sc = $this->getConfig->()->get("server-shortcut");
-    $tag = $this->getConfig()->get("server-tag");
-    $color = $this->getConfig()->get("server-color");
-    $mcolor= $this->getConfig()->get("sem-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   }
-
-   elseif(strtolower($cmd) == $sc){
-    $sc = $this->getConfig->()->get("console-shortcut");
-    $tag = $this->getConfig()->get("console-tag");
-    $color = $this->getConfig()->get("console-color");
-    $mcolor= $this->getConfig()->get("com-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor ." " . $msg);
-   }
-  
-   elseif(strtolower($cmd) == $sc){
-    $sc = $this->getConfig->()->get("bcia-shortcut");
-    $tag = $this->getConfig()->get("bcia-tag");
-    $color = $this->getConfig()->get("bcia-color");
-    $mcolor= $this->getConfig()->get("bcm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   }
-
-
- }
-  else{
-   $this->getServer()->broadcastMessage("§4You do not have permission");
+  switch($cmd){
+   case "broadcast":
+    
+    break;
+   case "bgod":
+    
+    break;
   }
-}
 
 
 
