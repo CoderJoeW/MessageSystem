aaa7521cb47bf321437bbf5dcd2eb8df9e69dd79
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-06 15:32:47 -0400

    Update main.php

--- src/FlamingGenius/MessageSystem/main.php
+++ src/FlamingGenius/MessageSystem/main.php
@@ -19,9 +19,14 @@ class main extends PluginBase{
 
  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
   $cmd = $command->getName();
+  $msg = implode(" " , $args);
   switch($cmd){
    case "broadcast":
-    
+    $sc = $this->getConfig()->get("broadcast-shortcut");
+    $tag = $this->getConfig()->get("broadcast-tag");
+    $color = $this->getConfig()->get("broadcast-color");
+    $mcolor = $this->getConfig()->get("brm-color");
+    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
     break;
    case "bgod":
     
