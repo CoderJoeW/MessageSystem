6e6214b2e39ea75bfba077636e742cca4d4c081f
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-07 14:56:50 -0400

    Update main.php

--- src/FlamingGenius/MessageSystem/main.php
+++ src/FlamingGenius/MessageSystem/main.php
@@ -20,49 +20,53 @@ class main extends PluginBase{
  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
   $cmd = $command->getName();
   $msg = implode(" " , $args);
-  switch($cmd){
-   case "broadcast":
-    $tag = $this->getConfig()->get("broadcast-tag");
-    $color = $this->getConfig()->get("broadcast-color");
-    $mcolor = $this->getConfig()->get("brm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+  $player = $this->getPlayer();
+  $permission = $this->getConfig()->get("default-permission");
+  if($player->hasPermission == $permission){
+   switch($cmd){
+    case "broadcast":
+     $tag = $this->getConfig()->get("broadcast-tag");
+     $color = $this->getConfig()->get("broadcast-color");
+     $mcolor = $this->getConfig()->get("brm-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
     break;
-   case "bgod":
-    $tag = $this->getConfig()->get("bgod-tag");
-    $color = $this->getConfig()->get("bgod-color");
-    $mcolor= $this->getConfig()->get("bgm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+    case "bgod":
+     $tag = $this->getConfig()->get("bgod-tag");
+     $color = $this->getConfig()->get("bgod-color");
+     $mcolor= $this->getConfig()->get("bgm-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
     break;
-   case "bhero":
-    $tag = $this->getConfig()->get("bhero-tag");
-    $color = $this->getConfig()->get("bhero-color");
-    $mcolor= $this->getConfig()->get("bhm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   break;
-   case "announce":
-    $tag = $this->getConfig()->get("announce-tag");
-    $color = $this->getConfig()->get("announce-color");
-    $mcolor= $this->getConfig()->get("anm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   break;
-   case "server":
-    $tag = $this->getConfig()->get("server-tag");
-    $color = $this->getConfig()->get("server-color");
-    $mcolor= $this->getConfig()->get("sem-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   break;
-   case "console":
-    $tag = $this->getConfig()->get("console-tag");
-    $color = $this->getConfig()->get("console-color");
-    $mcolor= $this->getConfig()->get("com-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor ." " . $msg);
-   break;
-   case "bcia":
-    $tag = $this->getConfig()->get("bcia-tag");
-    $color = $this->getConfig()->get("bcia-color");
-    $mcolor= $this->getConfig()->get("bcm-color");
-    $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
-   break;
+    case "bhero":
+     $tag = $this->getConfig()->get("bhero-tag");
+     $color = $this->getConfig()->get("bhero-color");
+     $mcolor= $this->getConfig()->get("bhm-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+    break;
+    case "announce":
+     $tag = $this->getConfig()->get("announce-tag");
+     $color = $this->getConfig()->get("announce-color");
+     $mcolor= $this->getConfig()->get("anm-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+    break;
+    case "server":
+     $tag = $this->getConfig()->get("server-tag");
+     $color = $this->getConfig()->get("server-color");
+     $mcolor= $this->getConfig()->get("sem-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+    break;
+    case "console":
+     $tag = $this->getConfig()->get("console-tag");
+     $color = $this->getConfig()->get("console-color");
+     $mcolor= $this->getConfig()->get("com-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor ." " . $msg);
+    break;
+    case "bcia":
+     $tag = $this->getConfig()->get("bcia-tag");
+     $color = $this->getConfig()->get("bcia-color");
+     $mcolor= $this->getConfig()->get("bcm-color");
+     $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
+    break;
+   }
   }
 
 
